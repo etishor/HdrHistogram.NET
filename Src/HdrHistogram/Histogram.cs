9f8eeafcefd9ea1b3016a1aac07ac19cec0ca8f0
Iulian Margarintescu <etishor@gmail.com>
2015-04-09 20:41:00 +0300

    add resharper profile to ensure file headers are correct

--- Src/HdrHistogram/Histogram.cs
+++ Src/HdrHistogram/Histogram.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Written by Gil Tene of Azul Systems, and released to the public domain,
+// as explained at http://creativecommons.org/publicdomain/zero/1.0/
+// 
+// Ported to .NET by Iulian Margarintescu under the same license and terms as the java version
+// Java Version repo: https://github.com/HdrHistogram/HdrHistogram
+// Latest ported version is available in the Java submodule in the root of the repo
+using System;
 using System.Runtime.CompilerServices;
 
 namespace HdrHistogram
