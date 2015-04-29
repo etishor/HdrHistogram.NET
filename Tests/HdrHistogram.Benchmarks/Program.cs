6032aa70b3961723a6834cf153c388a167a404da
Iulian Margarintescu <etishor@gmail.com>
2015-04-29 12:46:20 +0300

    add simple benchmark project

---
+++ Tests/HdrHistogram.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace HdrHistogram.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.DefaultMaxThreads = 1;
+            BenchmarkRunner.DefaultTotalSeconds = 5;
+
+            //BenchmarkRunner.Run("Noop", () => { });
+
+            var histogram = new Histogram(100, 2);
+            BenchmarkRunner.Run("Histogram", () => histogram.recordValue(1));
+
+        }
+    }
+}
